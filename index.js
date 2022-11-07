b581cf8a05c5d203489b0dc4694aee1824fa20d2
Ethan <64897642+Ethanol62@users.noreply.github.com>
2022-11-07 20:58:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,355 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Connecté sur ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'Quoi') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'quoi') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Qoi') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qoi') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**quoi**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**Quoi**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Pourquoi') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'pourquoi') {
+  msg.channel.send('feur');
+};
+if (msg.content === 'pk') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qwa') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Quoi?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'quoi?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Qoi?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qoi?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**quoi?**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**Quoi?**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Pourquoi?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'pourquoi?') {
+  msg.channel.send('feur?');
+};
+if (msg.content === 'pk?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qwa?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Quoi ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'quoi ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Qoi ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qoi ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'QUOI') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'QUOI ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**quoi ?**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === '**Quoi ?**') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'Pourquoi ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'pourquoi ?') {
+  msg.channel.send('feur?');
+};
+if (msg.content === 'pk ?') {
+    msg.channel.send('feur');
+  };
+  if (msg.content === 'qwa ?') {
+    msg.channel.send('feur');
+  };
+
+  if (msg.content === 'ah') {
+    msg.channel.send('bh');
+  };
+  if (msg.content === 'ahhh') {
+    msg.channel.send('bhhh');
+  };
+  if (msg.content === 'a') {
+    msg.channel.send('b');
+  };
+  if (msg.content === 'A') {
+    msg.channel.send('B');
+  };
+  if (msg.content === 'Ah') {
+    msg.channel.send('Bh');
+  };
+  if (msg.content === 'rouge') {
+    msg.channel.send('gorge');
+  };
+  if (msg.content === 'Rouge') {
+    msg.channel.send('Gorge');
+  };
+  if (msg.content === 'profonde') {
+    msg.channel.send('tg.');
+  };
+  if (msg.content === 'Profonde') {
+    msg.channel.send('tg.');
+  };
+
+  if (msg.content === 'tg.') {
+    msg.channel.send('espèce de gamin(e) immature.');
+  };
+  if (msg.content === 'oui') {
+    msg.channel.send('fi');
+  };
+  if (msg.content === 'Oui') {
+    msg.channel.send('Fi');
+  };
+  if (msg.content === 'ui') {
+    msg.channel.send('fi');
+  };
+  if (msg.content === 'non') {
+    msg.channel.send('bril');
+  };
+  if (msg.content === 'nn') {
+    msg.channel.send('bril');
+  };
+  if (msg.content === 'Hein ?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'HEIN ?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'HEIN?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'Hein?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'hein ?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'hein?!') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'hein') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'HEIN') {
+    msg.channel.send('deux');
+  };
+  if (msg.content === 'trois') {
+    msg.channel.send('continue sans moi');
+  };
+  if (msg.content === 'Trois') {
+    msg.channel.send('continue sans moi');
+  };
+  if (msg.content === 'Trois.') {
+    msg.channel.send('continue sans moi');
+  };
+  if (msg.content === 'trois.') {
+    msg.channel.send('continue sans moi');
+  };
+  if (msg.content === '3') {
+    msg.channel.send('continue sans moi');
+  };
+  if (msg.content === 'bah') {
+    msg.channel.send('ton');
+  };
+  if (msg.content === 'BAH') {
+    msg.channel.send('ton');
+  };
+  if (msg.content === 'Bah') {
+    msg.channel.send('ton');
+  };
+  if (msg.content === 'ba') {
+    msg.channel.send('ton');
+  };
+  if (msg.content === 'Ba') {
+    msg.channel.send('ton');
+  };
+  if (msg.content === "Qu'est ce qu'il y a ?") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu est ce qu il y a ?") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu est ce qu il y'a ?") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu est ce qu il y a ?") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu'est ce qu'il y a") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu est ce qu il y a") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "Qu est ce qu il y'a") {
+    msg.channel.send('on ne la fait pas à moi..');
+  };
+  if (msg.content === "chhh") {
+    msg.channel.send('tg.');
+  };
+  if (msg.content === "wsh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "wesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "Wesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "WESH") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "Ouesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "ouesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "Bah wesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "bah wesh") {
+    msg.channel.send('Ouesh ouesh canne à pêche');
+  };
+  if (msg.content === "ok") {
+    msg.channel.send('sur glace');
+  };
+  if (msg.content === "Ok") {
+    msg.channel.send('sur glace');
+  };
+  if (msg.content === "OK") {
+    msg.channel.send('sur glace');
+  };
+  if (msg.content === 'oe') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'OE') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'ouais') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'OUAIS') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'OUAIS !') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'ouais !') {
+    msg.channel.send('stern');
+  };
+  if (msg.content === 'ch') {
+    msg.channel.send('dh');
+  };
+  if (msg.content === 'eh') {
+    msg.channel.send('fh');
+  };
+  if (msg.content === 'gh') {
+    msg.channel.send('hh');
+  };
+  if (msg.content === 'ih') {
+    msg.channel.send('jh');
+  };
+  if (msg.content === 'kh') {
+    msg.channel.send('tg.');
+  };
+  if (msg.content === 'f!uptime') {
+    msg.delete();
+    let date = new Date(client.uptime)
+    let y = date.getFullYear() 
+    let mo = date.getMonth()
+    let dm = date.getDate()
+    let h = date.getHours()
+    let mi = date.getMinutes()
+    let s = date.getSeconds()
+    let ms = date.getMilliseconds()
+    
+    quantifie = (valeur,unite) => {
+      let texte = `${valeur} ${unite}`
+      if (valeur>1) texte = `${valeur} ${unite}s`
+      return texte
+    }
+    
+    y -= 1970 
+    dm -= 1 
+    h -= 1
+    
+    let t = [] 
+    if (y>0) t.push(`${y} ans`) 
+    if (mo>0) t.push(`${mo} mois`) 
+    if (dm>0)  t.push(quantifie(dm,"jour")) 
+    if (h>0)  t.push(quantifie(h,"heure")) 
+    if (mi>0)  t.push(quantifie(mi,"minute"))
+    if (s>0)  t.push(quantifie(s,"seconde"))
+    if (ms>0)  t.push(quantifie(ms,"milliseconde"))
+    let last = t.pop() 
+    t = t.join(", ") + ` et ${last}`
+    
+
+    const embederreur = new Discord.MessageEmbed()
+    .setDescription(`:x:・Seul mon créateur peut effectuer cette commande.`);
+
+    const embeduptime = new Discord.MessageEmbed()
+    .setDescription(`:timer:・Je suis en ligne depuis ${t}.`)
+    .setColor('#000000');
+
+    if (msg.author.id != "588439332491100170")
+    return msg.channel.send(embederreur)
+
+    msg.channel.send(embeduptime)
+  }});
+
+  client.on("ready", m => {
+    let sts = [
+     `faire le gamin`,
+      `faire le gamin`
+    ];
+    setInterval(function() {
+      
+      let sta = sts[Math.floor(Math.random() * sts.length)];
+      client.user.setActivity(sta, { type: "PLAYING" });
+   }, 10000);
+  });
+
+client.login("NjY5MjcwNzYyMTAzODk4MTIz.Gor4Wt.Pg2RTctc7cQa7NeFUZgA1n1iNkLD_wJD1bjYRE");
